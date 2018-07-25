6e9a6fc553b07665bed9e2090060f7d93341fdfe
Andrew Nichol <randrewnichol@berkeley.edu>
2018-07-24 19:36:46 -0700

    yes

--- information_retrieval/trie/main.cpp
+++ information_retrieval/trie/main.cpp
@@ -11,5 +11,9 @@ int main(){
 	//std::cout << temp << std::endl;
     int _id = 1;
     t.grow(temp, _id);
+	std::vector<int> response = t.find(temp);
+	for(int i = 0; i < response.size(); i++){
+		std::cout << response[i] << std::endl;	
+	}
     return 0;
 }
--- information_retrieval/trie/trie.h
+++ information_retrieval/trie/trie.h
@@ -22,6 +22,18 @@ public:
     }
 
     Node* rTraverse(std::vector<Node>* curr_leaf, std::string remainder){
+        // curr_char is remainder[0]
+        // 1. look through vector of nodes for match or surpass,
+        // if match, skip to 2 with matched node as {current_node}.
+        // elif surpasses node in vector:
+        //    insert node into place before node it surpassed. this new node is {current_node}. go to 2
+        // 2. peak forward to remainder[1]. 
+        // if null, 
+        //    if endpoint is false, set endpoint to true, allocate an empty vector of ints (doc ids) for this node. (add this to node class?), return pointer to node
+        //	  else: return pointer to node   
+		// return
+        // else call this function recursively with (children of {current_node}, remainder[1:] (how to do this?))	
+
 		char curr_char = remainder[0];
 		
 		// 1.
@@ -51,7 +63,6 @@ public:
         	(*curr_leaf).insert((*curr_leaf).begin(), newNode);
 			
 		} // clean this disgusting mess, u fagit
-		
 	
         Node* referenceNewNode = &((*curr_leaf)[i]);
 		//std::cout << "new node value : " << (*referenceNewNode).value << std::endl;
@@ -59,33 +70,48 @@ public:
 
 		// 2. 	
 		if(!remainder[1]){
-//			std::cout << "reached bottom yo. current cahracter is " << curr_char << std::endl;	
+			//std::cout << "reached bottom yo. current cahracter is " << curr_char << std::endl;	
             if(!(*referenceNewNode).endpoint){
                 (*referenceNewNode).endpoint = true;
             }
             return referenceNewNode;
 		}else{
 			remainder = remainder.substr(1, std::string::npos);
-			std::cout << "ABOUT TO CALL TRAVERSE WHILE ON CHAR " << curr_char << std::endl;
+			//std::cout << "ABOUT TO CALL TRAVERSE WHILE ON CHAR " << curr_char << std::endl;
 		    return rTraverse((*referenceNewNode).children, remainder);
 		}
-        // curr_char is remainder[0]
-        // 1. look through vector of nodes for match or surpass,
-        // if match, skip to 2 with matched node as {current_node}.
-        // elif surpasses node in vector:
-        //    insert node into place before node it surpassed. this new node is {current_node}. go to 2
-        // 2. peak forward to remainder[1]. 
-        // if null, 
-        //    if endpoint is false, set endpoint to true, allocate an empty vector of ints (doc ids) for this node. (add this to node class?), return pointer to node
-        //	  else: return pointer to node   
-		// return
-        // else call this function recursively with (children of {current_node}, remainder[1:] (how to do this?))
+
     }
 
     std::vector<int> find(std::string term){
-        std::cout << "find() function called" << std::endl; 
+		std::vector<Node>* curr_leaf = &root;
+		int i = 0;
+		char curr_char = term[i];
+	    while(curr_char){
+			int j = 0;
+			int length = (*curr_leaf).size();
+			while(j < length){
+				if((*curr_leaf)[j].value == curr_char){
+					break;
+				}	
+				j++;
+			}	
+			if(j < length){ // this means there was a match
+				if((*curr_leaf)[j].endpoint){
+					return (*curr_leaf)[j].doc_ids;	
+				}
+				curr_leaf = (*curr_leaf)[j].children;
+				i++;
+				curr_char = term[i];
+			}
+			else{
+				std::vector<int> empty;
+				return empty;
+			}
+		} // write this recursively? why? 	
+		std::vector<int> empty_return;
+		return empty_return;
     }
-
 };
 
 #define TRIE
